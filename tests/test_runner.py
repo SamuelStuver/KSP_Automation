23e2d79f7303f0a666d07f8d52f4f332b1fb9b36
Sam Stuver <sam_stuver@bose.com>
2019-08-23 15:47:23 -0600

    add test runner functionality

---
+++ tests/test_runner.py
@@ -0,0 +1,18 @@
+import pytest
+from enum import Enum
+
+class PytestExitCodes(Enum):
+    ALL_PASSING    = 0
+    SOME_FAILURES  = 1
+    USER_INTERRUPT = 2
+    INTERNAL_ERROR = 3
+    USAGE_ERROR    = 4
+    NO_TESTS_COLL  = 5
+
+
+def test_runner(directory):
+    result_code = pytest.main(args=['-svx', directory])
+    result = PytestExitCodes(result_code)
+    l
+
+if __name__ == "__main__":
